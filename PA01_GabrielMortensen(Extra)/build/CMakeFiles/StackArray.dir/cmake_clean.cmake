ce2c7aee7529e8712da92b7a2ddfc7f171af2b07
GMor10sen <58446889+GMor10sen@users.noreply.github.com>
2021-04-05 18:38:25 -0700

    Add files via upload

---
+++ PA01_GabrielMortensen(Extra)/build/CMakeFiles/StackArray.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StackArray.dir/src/StackArray.cpp.o"
+  "libStackArray.pdb"
+  "libStackArray.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StackArray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
